1e325c44cb5a498f8c4bbf51e275175b3067901e
Baiju shanzz <baijushanzz@gmail.com>
2023-10-25 22:16:04 +0530

    product models has to been created

--- accounts/models.py
+++ accounts/models.py
@@ -7,5 +7,15 @@ class Customer(models.Model):
     email = models.EmailField(null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-
+    def __str__(self):
+        return self.name
     
+
+class Product(models.Model):
+    CATEGORY = (('Indoor', 'Indoor'),
+    ('Out Door', 'Out Door'))
+    name = models.CharField(max_length=100)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=100,null=True,choices=CATEGORY)
+    description = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
