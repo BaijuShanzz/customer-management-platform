49fa6ea585ed86816098328bc15be02c4d646824
Baiju shanzz <baijushanzz@gmail.com>
2023-10-25 22:07:28 +0530

    customer model created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(max_length=10,null=True)
+    phone = models.BooleanField(null=True)
+    email = models.EmailField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+    
