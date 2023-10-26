6ba519a5372752c4f6ce1ba21bc193195855afbd
Baiju shanzz <baijushanzz@gmail.com>
2023-10-26 19:46:51 +0530

    Tag model created

--- accounts/models.py
+++ accounts/models.py
@@ -9,8 +9,13 @@ class Customer(models.Model):
 
     def __str__(self):
         return self.name
-    
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100,null=True)
+
+    def __str__(self):
+        return self.name
+    
 class Product(models.Model):
     CATEGORY = (('Indoor', 'Indoor'),
     ('Out Door', 'Out Door'))
@@ -19,3 +24,7 @@ class Product(models.Model):
     category = models.CharField(max_length=100,null=True,choices=CATEGORY)
     description = models.CharField(max_length=100)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
